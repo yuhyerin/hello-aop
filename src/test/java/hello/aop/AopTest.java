b276efbab9fb3b2d4c5ee06491eda22372330277
RiRi <37924810+yuhyerin@users.noreply.github.com>
2022-12-27 15:03:49 +0900

    AOP 테스트 (`@Aspect` 사용)

---
+++ src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,42 @@
+package hello.aop;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest
+@Import(AspectV1.class)
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo(){
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService)); // false -> @Import(AspectV1.class) 후 true
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository)); // false -> @Import(AspectV1.class) 후 true
+    }
+
+    @Test
+    void success(){
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception(){
+        Assertions.assertThatThrownBy(()-> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+}
